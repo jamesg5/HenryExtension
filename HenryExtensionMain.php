178525e8f8780c064ee32d3a9f9c60b09d12ae04
Henry Nguyen <nguyenhenryh1996@gmail.com>
2017-11-09 16:39:01 -0600

    Add files via upload

---
+++ HenryExtensionMain.php
@@ -0,0 +1,21 @@
+<?php
+class HenryExtention {
+
+	// Register any render callbacks with the parser
+    public static function onParserSetup( &$parser ) {
+
+    	// Create a function hook associating the "HenryExtension" magic word with render()
+    	$parser->setFunctionHook( 'henry', 'HenryExtension::render' );
+    }
+
+    // Render the output of {{#henry:}}.
+    public static function render( $parser, $param1 = '', $param2 = '', $param3 = '' ) {
+   		
+   		// The input parameters are wikitext with templates expanded.
+      	// The output should be wikitext too.
+   		$output = "param1 is $param1 and param2 is $param2 and param3 is $param3";
+
+   		return $output;
+    }
+
+}
