e5365069eb56d5a13474ead477f14bc7005221d5
Henry Nguyen <nguyenhenryh1996@gmail.com>
2017-11-09 16:26:54 -0600

    Add files via upload

---
+++ Magic.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @since 1.0.0
+ *
+ * @file
+ *
+ * @licence GNU GPL
+ * @author Henry Nguyen (hnguye24)
+ */
+
+$magicWords = array();
+
+/** English
+ * @author Henry Nguyen (hnguye24)
+ */
+$magicWords['en'] = array(
+   'henry' => array( 0, 'henry' ),
+);
